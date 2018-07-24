7e9872f438e93fede62c21f82c7d0a0b527a4c48
Bahrova Aleksandra <bagrovaalexandra@gmail.com>
2018-07-24 17:47:31 +0300

    all sections

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,27 @@
 $(function() {
+	// $(document).ready(function(){
+  //       $('.row.adwantages-content').slick({
+  //         speed: 300,
+	// 				infinite: true,
+  // 				slidesToShow: 3,
+  // 				slidesToScroll: 1,
+  //         prevArrow: '<span class="arrow left_arrow"><i class="fas fa-chevron-left"></i></span>',
+  //         nextArrow: '<span class="arrow right_arrow"><i class="fas fa-chevron-right"></i></span>',
+  //         appendArrows: '.adwantages'
+  //       });
+});
 
+$(function() {
+	var header = $(".header-pm");
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 200) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass('fixed');
+		}
+	});
 });
 
 $(window).on('load', function() {
